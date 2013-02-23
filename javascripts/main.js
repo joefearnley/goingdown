6c7afc6e68754176e207c519cb59ce3edd0f16b8
Joe Fearnley <joe.fearnley@gmail.com>
2013-02-23 18:51:11 -0500

    Updating plottiong options.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,28 +1,39 @@
-
-var placeholder = $('#placeholder');
-var data = [];
-var options = {
-  xaxis: {
-    mode: 'time',
-    timeformat: '%Y-%m-%d'
-  },
-  series: {
-    points: {
-      line: true,
-      show: true
+$(function () {
+  var placeholder = $('#placeholder');
+  var data = [];
+  var options = {
+    xaxis: {
+      mode: 'time',
+      timeformat: '%m-%d'
     }
+  };
+
+//var t = new Date('2012-02-12');
+//console.log(t);
+//console.log(t.getTime());
+
+  for(i = 0; i < weightData.length; i++) {
+    var time = new Date(weightData[i].date).getTime();
+    var pounds = parseFloat(weightData[i].pounds);
+
+    var d = [time, pounds];
+
+    data.push(d);
   }
-};
 
-//console.log(data);
+  console.log(data);
 
-
-for(i = 0; i < weightData.length; i++) {
-
-  var d = [weightData[i].pounds, weightData[i].date];
-  data.push(d);
-}
-
-console.log(data);
-
-$.plot(placeholder, data, options);
+  $.plot(placeholder, 
+    [
+      {
+        data: data,
+        points: {
+          show: true
+        },
+        lines: {
+          show: true
+        }
+      }
+    ],
+    options);
+});
