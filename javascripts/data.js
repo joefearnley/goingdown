97da0a852b10745cab0a753c10f744daf2aad4b2
Joe Fearnley <joe.fearnley@gmail.com>
2013-02-23 16:03:52 -0500

    Adding sample data.

---
+++ javascripts/data.js
@@ -0,0 +1,202 @@
+var weightData = [
+	{
+		"date": "2012-04-09",
+		"kilograms": "83.9",
+		"pounds": "185.0"
+	},
+	{
+		"date": "2012-04-11",
+		"kilograms": "83.5",
+		"pounds": "184.0"
+	},
+	{
+		"date": "2012-04-13",
+		"kilograms": "82.6",
+		"pounds": "182.2"
+	},
+	{
+		"date": "2012-04-24",
+		"kilograms": "82.3",
+		"pounds": "181.5"
+	},
+	{
+		"date": "2012-04-27",
+		"kilograms": "81.2",
+		"pounds": "179.1"
+	},
+	{
+		"date": "2012-05-04",
+		"kilograms": "80.5",
+		"pounds": "177.4"
+	},
+	{
+		"date": "2012-05-11",
+		"kilograms": "79.5",
+		"pounds": "175.3"
+	},
+	{
+		"date": "2013-01-09",
+		"kilograms": "83.2",
+		"pounds": "183.5"
+	},
+	{
+		"date": "2013-01-10",
+		"kilograms": "82.2",
+		"pounds": "181.2"
+	},
+	{
+		"date": "2013-01-11",
+		"kilograms": "81.6",
+		"pounds": "179.8"
+	},
+	{
+		"date": "2013-01-13",
+		"kilograms": "82.3",
+		"pounds": "181.5"
+	},
+	{
+		"date": "2013-01-14",
+		"kilograms": "81.2",
+		"pounds": "179.1"
+	},
+	{
+		"date": "2013-01-16",
+		"kilograms": "82.2",
+		"pounds": "181.2"
+	},
+	{
+		"date": "2013-01-22",
+		"kilograms": "82.6",
+		"pounds": "182.2"
+	},
+	{
+		"date": "2013-01-23",
+		"kilograms": "81.7",
+		"pounds": "180.2"
+	},
+	{
+		"date": "2013-01-25",
+		"kilograms": "80.1",
+		"pounds": "176.6"
+	},
+	{
+		"date": "2013-01-26",
+		"kilograms": "79.8",
+		"pounds": "176.0"
+	},
+	{
+		"date": "2013-01-28",
+		"kilograms": "81.2",
+		"pounds": "179.0"
+	},
+	{
+		"date": "2013-01-29",
+		"kilograms": "81.1",
+		"pounds": "178.8"
+	},
+	{
+		"date": "2013-01-30",
+		"kilograms": "80.8",
+		"pounds": "178.2"
+	},
+	{
+		"date": "2013-01-31",
+		"kilograms": "80.4",
+		"pounds": "177.2"
+	},
+	{
+		"date": "2013-02-01",
+		"kilograms": "79.7",
+		"pounds": "175.8"
+	},
+	{
+		"date": "2013-02-04",
+		"kilograms": "81.2",
+		"pounds": "179.0"
+	},
+	{
+		"date": "2013-02-05",
+		"kilograms": "81.0",
+		"pounds": "178.6"
+	},
+	{
+		"date": "2013-02-06",
+		"kilograms": "80.7",
+		"pounds": "178.0"
+	},
+	{
+		"date": "2013-02-07",
+		"kilograms": "80.3",
+		"pounds": "177.0"
+	},
+	{
+		"date": "2013-02-08",
+		"kilograms": "79.5",
+		"pounds": "175.3"
+	},
+	{
+		"date": "2013-02-09",
+		"kilograms": "78.6",
+		"pounds": "173.2"
+	},
+	{
+		"date": "2013-02-11",
+		"kilograms": "79.1",
+		"pounds": "174.4"
+	},
+	{
+		"date": "2013-02-12",
+		"kilograms": "79.6",
+		"pounds": "175.4"
+	},
+	{
+		"date": "2013-02-13",
+		"kilograms": "78.7",
+		"pounds": "173.6"
+	},
+	{
+		"date": "2013-02-14",
+		"kilograms": "78.6",
+		"pounds": "173.2"
+	},
+	{
+		"date": "2013-02-15",
+		"kilograms": "77.7",
+		"pounds": "171.2"
+	},
+	{
+		"date": "2013-02-16",
+		"kilograms": "77.3",
+		"pounds": "170.4"
+	},
+	{
+		"date": "2013-02-17",
+		"kilograms": "77.8",
+		"pounds": "171.5"
+	},
+	{
+		"date": "2013-02-18",
+		"kilograms": "77.7",
+		"pounds": "171.3"
+	},
+	{
+		"date": "2013-02-19",
+		"kilograms": "78.7",
+		"pounds": "173.4"
+	},
+	{
+		"date": "2013-02-20",
+		"kilograms": "78.2",
+		"pounds": "172.5"
+	},
+	{
+		"date": "2013-02-21",
+		"kilograms": "77.7",
+		"pounds": "171.4"
+	},
+	{
+		"date": "2013-02-22",
+		"kilograms": "77.3",
+		"pounds": "170.5"
+	}
+];
