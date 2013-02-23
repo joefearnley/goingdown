550cfc6ac5426482291505b495295f1fb7a1a0dc
Joe Fearnley <joe.fearnley@gmail.com>
2013-02-23 13:43:22 -0500

    Adding initial plotting code.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,9 @@
-console.log('This would be the main JS file.');
+//console.log('This would be the main JS file.');
+//
+//
+
+var placeholder = $('#placeholder');
+var data = {};
+var options = {};
+
+$.plot(placeholder, data, options);
