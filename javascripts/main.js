dc6820ab16789c07609b37904de803c6961494ae
Joe Fearnley <joe.fearnley@gmail.com>
2013-02-23 16:05:16 -0500

    Updating js includes on main index.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,28 @@
-//console.log('This would be the main JS file.');
-//
-//
 
 var placeholder = $('#placeholder');
-var data = {};
-var options = {};
+var data = [];
+var options = {
+  xaxis: {
+    mode: 'time',
+    timeformat: '%Y-%m-%d'
+  },
+  series: {
+    points: {
+      line: true,
+      show: true
+    }
+  }
+};
+
+//console.log(data);
+
+
+for(i = 0; i < weightData.length; i++) {
+
+  var d = [weightData[i].pounds, weightData[i].date];
+  data.push(d);
+}
+
+console.log(data);
 
 $.plot(placeholder, data, options);
