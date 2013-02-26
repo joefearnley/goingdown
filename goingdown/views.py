f7fe8e60122f6ee87556157c68dd358796d79bb5
Joe Fearnley <joe.fearnley@gmail.com>
2013-02-25 19:07:36 -0500

    Adding commments to views.py

--- goingdown/views.py
+++ goingdown/views.py
@@ -5,34 +5,32 @@ import os.path
 import json
 import csv
 
-@app.route("/")
+@app.route('/')
 def index():
+    """Return weight data as json. Check to see if there is a csv file 
+    in the format of weightbot_data_DATE.csv in the /data directory. If 
+    it does not call weightbot.com and download it.
+    """
 
     today = datetime.datetime.now().strftime('%m-%d-%Y')
     filename = 'data/weightbot_data_%s.csv'  % today
     data = ''
 
-    # check for a file with a timestamp of today
     if os.path.isfile(filename) == False :
-
-        # if one is not found call weightbot to get data
         username = app.config['WEIGHTBOT_USERNAME']
         password = app.config['WEIGHTBOT_PASSWORD']
         bot = WeightBot(username, password)
         data = bot.get_csv_data()
 
-        # write this to a file.
         f = open(filename, 'w')
         f.write(data)
         f.close()
 
-    # open the file and read data
     f = open(filename, 'r')
     reader = csv.DictReader(f, fieldnames = ('date', 'kilograms', 'pounds'))
     response = json.dumps([ row for row in reader ])
     f.close()
 
-    # print json data to response
     return response
 
 if __name__ == "__main__":
