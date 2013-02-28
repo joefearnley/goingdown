a3d01f1cf2dc571b6397b169c8d6c6bfdf1fcf03
Joe Fearnley <joe.fearnley@gmail.com>
2013-02-27 21:55:48 -0500

    Adding CORS compliance to views.py.

--- goingdown/views.py
+++ goingdown/views.py
@@ -1,11 +1,12 @@
 from weightbot import WeightBot
 from goingdown import app
+from flask import request, make_response
 import datetime
 import os.path
 import json
 import csv
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
     """Return weight data as json. Check to see if there is a csv file 
     in the format of weightbot_data_DATE.csv in the /data directory. If 
@@ -28,8 +29,13 @@ def index():
 
     f = open(filename, 'r')
     reader = csv.DictReader(f, fieldnames = ('date', 'kilograms', 'pounds'))
-    response = json.dumps([ row for row in reader ])
+    data = json.dumps([ row for row in reader ])
     f.close()
 
+    allowed = ['http://localhost', 'http://goingdown.heroku.com']
+    response = make_response(data)
+    origin = request.headers.get('Origin')
+    if origin in allowed:
+        response.headers['Access-Control-Allow-Origin'] = origin
+  
     return response
-
