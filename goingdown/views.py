f180d67ac462d10147dff58537f1a061e35e2c2a
Joe Fearnley <joe.fearnley@gmail.com>
2013-03-04 22:38:28 -0500

    Adding crossdomain decorator.

--- goingdown/views.py
+++ goingdown/views.py
@@ -1,12 +1,14 @@
 from weightbot import WeightBot
 from goingdown import app
 from flask import request, make_response
+from crossdomain import crossdomain
 import datetime
 import os.path
 import json
 import csv
 
 @app.route('/', methods=['GET'])
+@crossdomain(origin='*')
 def index():
     """Return weight data as json. Check to see if there is a csv file 
     in the format of weightbot_data_DATE.csv in the /data directory. If 
@@ -34,8 +36,4 @@ def index():
 
     allowed = ['http://localhost', 'http://joefearnley.com/goingdown']
     response = make_response(data)
-    origin = request.headers.get('Origin')
-    if origin in allowed:
-        response.headers['Access-Control-Allow-Origin'] = origin
-
     return response
