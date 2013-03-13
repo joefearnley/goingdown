49a58aa0fd064a007ebc57e19676abcc9e6b01e1
Joe Fearnley <joe.fearnley@gmail.com>
2013-03-12 22:31:01 -0400

    Getting ajax stuff working.

--- javascripts/main.js
+++ javascripts/main.js
@@ -18,17 +18,22 @@ $(function () {
 
   var options = {
     type: 'GET',
-    url: 'http://goingdown.heroku.com',
-    crossDomain: true
+    //url: 'http://goingdown.heroku.com',
+    url: 'http://localhost:5000',
+    crossDomain: true,
+    dataType: 'json'
+    //xhrFields: {
+    //  withCredentials: true
+    //},
   };
 
   var request = $.ajax(options);
 
   request.done(function(data, textStatus, jqXHR) {
     console.log('Ajax call sucessful...updating graph.');
-    for(i = 0; i < weightData.length; i++) {
-      var time = new Date(weightData[i].date).getTime();
-      var pounds = parseFloat(weightData[i].pounds);
+    for(i = 0; i < data.length; i++) {
+      var time = new Date(data[i].date).getTime();
+      var pounds = parseFloat(data[i].pounds);
       var d = [time, pounds];
       data.push(d);
     }
@@ -46,7 +51,3 @@ $(function () {
     $('#loading').hide();
   });
 });
-
-
-
-
