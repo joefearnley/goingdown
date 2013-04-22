966c3661547ebb7f0c302bf68f458b4f5a998a57
Joe Fearnley <joe.fearnley@gmail.com>
2013-04-22 12:27:36 -0300

    Adding attribution to crossdomain.py.

--- goingdown/crossdomain.py
+++ goingdown/crossdomain.py
@@ -5,6 +5,11 @@ from functools import update_wrapper
 def crossdomain(origin=None, methods=None, headers=None,
                 max_age=21600, attach_to_all=True,
                 automatic_options=True):
+    """Decorator for the HTTP Access Control
+    Found here:
+    http://flask.pocoo.org/snippets/56/
+    
+    """
     if methods is not None:
         methods = ', '.join(sorted(x.upper() for x in methods))
     if headers is not None and not isinstance(headers, basestring):
